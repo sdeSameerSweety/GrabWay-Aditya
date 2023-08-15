4a64ef658477345c981435aecfb855da9e25ab08
Aditya Kumar <ceo@sameerswain.com>
2023-08-16 02:59:17 +0530

    TopBar Ready

--- grabway/src/Home.js
+++ grabway/src/Home.js
@@ -1,25 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
-
-function Home() {
+import React from 'react'
+import TopBar from './components/Navbar/TopNavBar/TopBar';
+const Home = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TopBar/>
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Home
