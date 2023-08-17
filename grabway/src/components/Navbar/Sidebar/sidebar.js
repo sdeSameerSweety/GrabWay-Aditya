0fb8aa0f2e84a14bebba2df7bd5a1cae851978af
Aditya Kumar <ceo@sameerswain.com>
2023-08-17 22:41:36 +0530

    minor bugs

--- grabway/src/components/Navbar/Sidebar/sidebar.js
+++ grabway/src/components/Navbar/Sidebar/sidebar.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import "./sidebar.css";
-import { Switch, Divider } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 export default function Sidebar({ classDisplay, setClassDisplay }) {
