896eaffbb28e670bd036c66b7cdf2c9ce2d328f8
Aditya Kumar <ceo@sameerswain.com>
2023-08-18 21:57:55 +0530

    sidebar full responsivness in desktop and tab view completed

--- grabway/src/App.js
+++ grabway/src/App.js
@@ -1,21 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import TopSection from "./components/HomePage/TopSection";
 import Footer from "./components/Footer/footer";
 import TopScroller from "./components/TopScroller/TopSlider";
 function App() {
+  const [windowSize, setWindowSize] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowSize(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleWindowResize);
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
   const [classDisplay, setClassDisplay] = useState("sidebar");
   const intDivStyle = {
     position: "relative",
-    marginLeft: classDisplay === "sidebar" ? "78px" : "250px",
+    marginLeft:
+      windowSize > 600
+        ? classDisplay === "sidebar"
+          ? "78px"
+          : "255px"
+        : "0px",
     transition: "all 0.5s ease",
   };
+
+  console.log(classDisplay);
   return (
     <BrowserRouter>
       <Navbar classDisplay={classDisplay} setClassDislay={setClassDisplay} />
       <div style={intDivStyle}>
-        <TopScroller/>
+        <TopScroller />
         <Routes>
           <Route path="/" element={<TopSection />} />
         </Routes>
--- grabway/src/components/Navbar/Sidebar/sidebar.js
+++ grabway/src/components/Navbar/Sidebar/sidebar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./sidebar.css";
 import { Link } from "react-router-dom";
 
@@ -18,91 +18,123 @@ export default function Sidebar({ classDisplay, setClassDisplay }) {
     console.log("Signout Successfull");
   };
 
+  const [windowSize, setWindowSize] = useState([
+    window.innerWidth,
+    window.innerHeight,
+  ]);
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowSize([window.innerWidth, window.innerHeight]);
+    };
+    window.addEventListener("resize", handleWindowResize);
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
+  function handleSmallChange() {
+    setClassDisplay("sidebar");
+    return "sidebar";
+  }
+
+  function handleBigChange() {
+    setClassDisplay(sidebarClass);
+    return sidebarClass;
+  }
   return (
     <>
-      <div class={sidebarClass}>
-        <div class="logo-details">
-          <i
-            class={sidebarClass === "sidebar" ? "bx bx-menu" : "bx bx-x-circle"}
-            style={{ color: "black" }}
-            id="btn"
-            onClick={toggleClass}
-          ></i>
-        </div>
-        <ul class="nav-list">
-          <li>
-            <Link to="/">
-              <div className="items-list-side-book-ride">
-                <i class="bx bx-car" style={{ color: "#000000" }}></i>
-                <span class="links_name">Book Ride</span>
-              </div>
-              <span class="tooltip">Book Ride</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/">
-              <div className="items-list-side">
-                <i class="bx bx-grid-alt"></i>
-                <span class="links_name">Dashboard</span>
-              </div>
-              <span class="tooltip">Dashboard</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/">
-              <div className="items-list-side">
-                <i class="bx bx-user"></i>
-                <span class="links_name">Ride History</span>
-              </div>
-              <span class="tooltip">Ride History</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/">
-              <div className="items-list-side">
-                <i class="bx bx-pie-chart-alt-2"></i>
-                <span class="links_name">Analytics</span>
-              </div>
-              <span class="tooltip">Analytics</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/">
-              <div className="items-list-side">
-                <i class="bx bx-chat"></i>
-                <span class="links_name">Packages</span>
-              </div>
-              <span class="tooltip">Packages</span>
-            </Link>
-          </li>
-
-          <li>
-            <Link to="/">
-              <div className="items-list-side">
-                <i class="bx bx-folder"></i>
-                <span class="links_name">Support</span>
-              </div>
-              <span class="tooltip">Support</span>
-            </Link>
-          </li>
-
-          <li class="profile">
-            <div class="profile-details">
-              <img src="/assets/images/user.png" alt="profileImg" />
-              <div class="name_job">
-                <div class="name">Demo Person</div>
-                <div class="job">Customer</div>
-              </div>
+      {windowSize[0] > 600 && (
+        <div
+          class={windowSize[0] > 900 ? handleBigChange() : handleSmallChange()}
+        >
+          {windowSize[0] > 900 && (
+            <div class="logo-details">
+              <i
+                class={
+                  sidebarClass === "sidebar" ? "bx bx-menu" : "bx bx-x-circle"
+                }
+                style={{ color: "black" }}
+                id="btn"
+                onClick={toggleClass}
+              ></i>
             </div>
-            <i
-              class="bx bx-log-out"
-              id="log_out"
-              role="button"
-              onClick={handleSignout}
-            ></i>
-          </li>
-        </ul>
-      </div>
+          )}
+          <ul class="nav-list">
+            <li>
+              <Link to="/">
+                <div className="items-list-side-book-ride">
+                  <i class="bx bx-car" style={{ color: "#000000" }}></i>
+                  <span class="links_name">Book Ride</span>
+                </div>
+                <span class="tooltip">Book Ride</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/">
+                <div className="items-list-side">
+                  <i class="bx bx-grid-alt"></i>
+                  <span class="links_name">Dashboard</span>
+                </div>
+                <span class="tooltip">Dashboard</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/">
+                <div className="items-list-side">
+                  <i class="bx bx-user"></i>
+                  <span class="links_name">Ride History</span>
+                </div>
+                <span class="tooltip">Ride History</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/">
+                <div className="items-list-side">
+                  <i class="bx bx-pie-chart-alt-2"></i>
+                  <span class="links_name">Analytics</span>
+                </div>
+                <span class="tooltip">Analytics</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/">
+                <div className="items-list-side">
+                  <i class="bx bx-chat"></i>
+                  <span class="links_name">Packages</span>
+                </div>
+                <span class="tooltip">Packages</span>
+              </Link>
+            </li>
+
+            <li>
+              <Link to="/">
+                <div className="items-list-side">
+                  <i class="bx bx-folder"></i>
+                  <span class="links_name">Support</span>
+                </div>
+                <span class="tooltip">Support</span>
+              </Link>
+            </li>
+
+            <li class="profile">
+              <div class="profile-details">
+                <img src="/assets/images/user.png" alt="profileImg" />
+                <div class="name_job">
+                  <div class="name">Demo Person</div>
+                  <div class="job">Customer</div>
+                </div>
+              </div>
+              <i
+                class="bx bx-log-out"
+                id="log_out"
+                role="button"
+                onClick={handleSignout}
+              ></i>
+            </li>
+          </ul>
+        </div>
+      )}
     </>
   );
 }
