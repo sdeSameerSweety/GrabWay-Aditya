cb083ae23f8e4e178d3884b824c1ce01e64084c6
Aditya Kumar <ceo@sameerswain.com>
2023-08-21 02:08:16 +0530

    Work done

--- grabway/src/Pages/HomePage/TopScroller/TopSlider.jsx
+++ grabway/src/Pages/HomePage/TopScroller/TopSlider.jsx
@@ -11,21 +11,22 @@ import "./TopSlider.css";
 
 export default function TopSlider() {
   const [slideIndex, setSlideIndex] = useState(0);
+  const sidebarWidth = useBreakpointValue({ base: "100%", md: "100%",sm: "none" });
 
   const slides = [
     {
       image:
-        "",
+        "https://media.istockphoto.com/id/1205122850/vector/modern-grandmother-driving-sports-car-on-city-road.jpg?s=612x612&w=0&k=20&c=3L3UrKzl8wjTaRfwFZq5qKl-xOkzZMt_vwkK3wA22IA=",
       text: "Slide 1: Kaisa hai Aditya?",
     },
     {
       image:
-        "",
+        "https://media.istockphoto.com/id/1179640327/vector/group-of-people-sharing-car-online-car-sharing-service-vector-happy-friend-with-multi-ethnic.jpg?s=612x612&w=0&k=20&c=22qYP3RU42CY_dZ22YgeToTU527eEnJtvo-8YOZDNFQ=",
       text: "Slide 2: Kya hal chal Kittu",
     },
     {
       image:
-        "",
+        "https://media.istockphoto.com/id/1163993432/vector/car-sharing-illustration-young-people-are-ready-to-move-off.jpg?s=612x612&w=0&k=20&c=u4Mjn7Aa4OCQJv-fzSR6oCxz99SeZdyCiU5Ffbn7jP4=",
       text: "Slide 3: Rajiv Mast hai",
     },
   ];
@@ -50,37 +51,37 @@ export default function TopSlider() {
     };
   }, [slideIndex]);
 
-  // ... (previous code)
-
   return (
-    <Flex className="slider">
-      <VStack
-        w={"full"}
-        justify={"center"}
-        px={useBreakpointValue({ base: 4, md: 8 })}
-        rounded="1.5rem"
-        bgGradient={"linear(to-r, blackAlpha.600, transparent)"}
-        position="relative"
-      >
-        <Image
-          src={slides[slideIndex].image}
-          alt={`Slide ${slideIndex + 1}`}
-          borderRadius="1.5rem"
-          boxShadow="xl"
-        />
-        <Text
-          position="absolute"
-          bottom="2rem"
-          left="2rem"
-          color={"white"}
-          fontWeight={700}
-          lineHeight={1.2}
-          fontSize={useBreakpointValue({ base: "3xl", md: "4xl" })}
-          zIndex={1}
+    <VStack w={sidebarWidth}>
+      <Flex className="slider">
+        <VStack
+          w={"full"}
+          justify={"center"}
+          px={useBreakpointValue({ base: 4, md: 8 })}
+          rounded="1.5rem"
+          bgGradient={"linear(to-r, blackAlpha.600, transparent)"}
+          position="relative"
         >
-          {slides[slideIndex].text}
-        </Text>
-        <Button
+          <Image
+            src={slides[slideIndex].image}
+            alt={`Slide ${slideIndex + 1}`}
+            borderRadius="1.5rem"
+            boxShadow="xl"
+            maxWidth="100%"
+          />
+          <Text
+            position="absolute"
+            bottom="2rem"
+            left="2rem"
+            color={"white"}
+            fontWeight={700}
+            lineHeight={1.2}
+            fontSize={useBreakpointValue({ base: "3xl", md: "4xl" })}
+            zIndex={1}
+          >
+            {slides[slideIndex].text}
+          </Text>
+          {/* <Button
           onClick={prevSlide}
           bg={"whiteAlpha.300"}
           rounded={"full"}
@@ -103,21 +104,22 @@ export default function TopSlider() {
           right="6rem"
         >
           Next
-        </Button>
-        <Button
-          onClick={handleButtonClick} // Driver click login
-          bg={"green.400"}
-          rounded={"full"}
-          color={"white"}
-          _hover={{ bg: "green.500" }}
-          position="absolute"
-          bottom="1rem"
-          left="50%"
-          transform="translateX(-50%)"
-        >
-          Action Button
-        </Button>
-      </VStack>
-    </Flex>
+        </Button> */}
+          <Button
+            onClick={null} // Driver click to signup
+            bg={"green.400"}
+            rounded={"full"}
+            color={"white"}
+            _hover={{ bg: "green.500" }}
+            position="absolute"
+            bottom="1rem"
+            left="50%"
+            transform="translateX(-50%)"
+          >
+            Action Button
+          </Button>
+        </VStack>
+      </Flex>
+    </VStack>
   );
 }
