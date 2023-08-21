45b23c6469634e0a9f536c7e2b9bf6a1aecda9ec
Aditya Kumar <ceo@sameerswain.com>
2023-08-21 09:20:51 +0530

    Revert "Revert "Slider Completed""

--- grabway/src/Pages/HomePage/TopScroller/TopSlider.jsx
+++ grabway/src/Pages/HomePage/TopScroller/TopSlider.jsx
@@ -1,32 +1,32 @@
 import React, { useState, useEffect } from "react";
 import {
-  Stack,
   Flex,
   Button,
   Text,
   VStack,
   useBreakpointValue,
+  Image,
 } from "@chakra-ui/react";
-import "./TopScroller.css"; // Import your CSS file
+import "./TopSlider.css";
 
-export default function TopScroller() {
+export default function TopSlider() {
   const [slideIndex, setSlideIndex] = useState(0);
 
   const slides = [
     {
       image:
-        "https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      text: "Slide 1: Kaisa hai Aditya?",
+        "https://static.vecteezy.com/system/resources/previews/007/523/317/non_2x/travaling-of-sport-roster-car-driving-on-tha-asphalt-road-path-sized-a-ocean-beach-with-sandy-beaches-and-a-background-of-island-under-a-blue-sky-illustrator-and-for-summer-posters-vector.jpg",
+      text: "1. Drive, Earn, and Share the Ride with Grabway Carpooling!",
     },
     {
       image:
-        "https://images.pexels.com/photos/808465/pexels-photo-808465.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      text: "Slide 2: Kya hal chal Kittu",
+        "https://img.freepik.com/premium-vector/big-semi-truck-trailer-driving-coutryside-road-nature-landscape-horizontal-banner_48369-13353.jpg?w=2000",
+      text: "2. Cut Costs, Connect, and Contribute: Grabway Carpooling!",
     },
     {
       image:
-        "https://images.pexels.com/photos/808465/pexels-photo-808465.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      text: "Slide 3: Rajiv Mast hai",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAp-a2nDcV-73s-VEmgO85PvjZQGkwE6PSPA&usqp=CAU",
+      text: "3. Carpooling Redefined: Grabway - Your Daily Solution!",
     },
   ];
 
@@ -43,7 +43,7 @@ export default function TopScroller() {
   useEffect(() => {
     const interval = setInterval(() => {
       nextSlide();
-    }, 3000); //timer
+    }, 5000); //timer
 
     return () => {
       clearInterval(interval);
@@ -55,40 +55,74 @@ export default function TopScroller() {
       <VStack
         w={"full"}
         justify={"center"}
-        px={useBreakpointValue({ base: 4, md: 8 })}
+        // px={useBreakpointValue({ base: 2, md: 1 })}
+        // py={useBreakpointValue({ base: 2, md: 1 })}
         rounded="1.5rem"
-        bgGradient={"linear(to-r, blackAlpha.600, transparent)"}
+        bg={"transparent"}
+        position="relative"
       >
-        <Stack maxW={"2xl"} align={"flex-start"} spacing={6}>
-          <Text
-            color={"white"}
-            fontWeight={700}
-            lineHeight={1.2}
-            fontSize={useBreakpointValue({ base: "3xl", md: "4xl" })}
-          >
-            {slides[slideIndex].text}
-          </Text>
-          <Stack direction={"row"}>
-            <Button
-              onClick={nextSlide}
-              bg={"blue.400"}
-              rounded={"full"}
-              color={"white"}
-              _hover={{ bg: "blue.500" }}
-            >
-              Show me more
-            </Button>
-            <Button
-              onClick={prevSlide}
-              bg={"whiteAlpha.300"}
-              rounded={"full"}
-              color={"white"}
-              _hover={{ bg: "whiteAlpha.500" }}
-            >
-              Show me more
-            </Button>
-          </Stack>
-        </Stack>
+        <Image
+          src={slides[slideIndex].image}
+          alt={`Slide ${slideIndex + 1}`}
+          borderRadius="0"
+          boxShadow="sm"
+          maxWidth="100%"
+          maxH={"100%"}
+          w="100%"
+          h="auto"
+          objectFit={"fill"}
+        />
+        <div className="dark-overlay"></div>
+        <Text
+          position="absolute"
+          bottom="4rem"
+          left="3rem"
+          color={"white"}
+          fontWeight={700}
+          lineHeight={1.5}
+          fontSize={useBreakpointValue({ base: "2xl", md: "3xl" })}
+          zIndex={1}
+        >
+          {slides[slideIndex].text}
+        </Text>
+        {/* <Button
+          onClick={prevSlide}
+          bg={"whiteAlpha.300"}
+          rounded={"full"}
+          color={"white"}
+          _hover={{ bg: "whiteAlpha.500" }}
+          position="absolute"
+          bottom="1rem"
+          right="1rem"
+        >
+          Previous
+        </Button>
+        <Button
+          onClick={nextSlide}
+          bg={"blue.400"}
+          rounded={"full"}
+          color={"white"}
+          _hover={{ bg: "blue.500" }}
+          position="absolute"
+          bottom="1rem"
+          right="6rem"
+        >
+          Next
+        </Button> */}
+        <Button
+          onClick={null} // Driver click to signup
+          bg={"red.500"}
+          rounded={"full"}
+          color={"white"}
+          _hover={{ bg: "green.500" }}
+          position="absolute"
+          bottom="1rem"
+          left="13%"
+          transform="translateX(-50%)"
+          shadow={2}
+        >
+          Join Now
+        </Button>
       </VStack>
     </Flex>
   );
