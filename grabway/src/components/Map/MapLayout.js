47b16d788645a4fd26ae6cdb1bdca2e60d18d635
Aditya Kumar <ceo@sameerswain.com>
2023-08-22 00:12:43 +0530

    commit

--- grabway/src/components/Map/MapLayout.js
+++ grabway/src/components/Map/MapLayout.js
@@ -1,13 +1,51 @@
-import React from "react";
-import { useJsApiLoader } from "@react-google-maps/api";
-const MapLayout = () => {
-  const { isLoaded } = useJsApiLoader({
-    googleMapsApiKey: "AIzaSyAfuFBhydCeCnk1Kl1c6u_1SfrIyXlReh0",
-  });
+import React from 'react'
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
-  if (!isLoaded) {
-    return <>Loading...</>;
-  }
+const containerStyle = {
+  width: '1200px',
+  height:'1200px'
+  
 };
 
-export default MapLayout;
+const center = {
+  lat: -22,
+  lng: 22
+};
+
+function MyComponent() {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "AIzaSyDJaFr-HFXGBOg8pUSdQfGjGwGdIwtbXhY",
+    libraries:['maps']
+  })
+
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  return isLoaded ? (
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={5}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+        
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
+  ) : <></>
+}
+
+export default React.memo(MyComponent)
