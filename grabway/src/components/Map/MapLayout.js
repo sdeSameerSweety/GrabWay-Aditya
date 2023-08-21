f6e53fc0481ee605d57f8e53b0bd3ff4f41a7ead
Aditya Kumar <ceo@sameerswain.com>
2023-08-21 21:46:50 +0530

    dashboard bug fix

--- grabway/src/components/Map/MapLayout.js
+++ grabway/src/components/Map/MapLayout.js
@@ -1,15 +1,13 @@
-import React from 'react'
-import { useJsApiLoader } from '@react-google-maps/api'
+import React from "react";
+import { useJsApiLoader } from "@react-google-maps/api";
 const MapLayout = () => {
- 
-    const {isLoaded}=useJsApiLoader({
-      googleMapsApiKey:"AIzaSyAfuFBhydCeCnk1Kl1c6u_1SfrIyXlReh0",
-      libraries:['places'],
-    }) 
-    
-    if(!isLoaded){
-      return <>Loading...</>
-    }
-  }
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: "AIzaSyAfuFBhydCeCnk1Kl1c6u_1SfrIyXlReh0",
+  });
 
-export default MapLayout
+  if (!isLoaded) {
+    return <>Loading...</>;
+  }
+};
+
+export default MapLayout;
