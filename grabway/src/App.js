a4506caf9cf5c2bb77d9a86c36b42c4122e2bd12
Aditya Kumar <ceo@sameerswain.com>
2023-08-26 15:55:28 +0530

    Minor changes

--- grabway/src/App.js
+++ grabway/src/App.js
@@ -12,6 +12,10 @@ import axios from "axios";
 import { UserContext, UserContextProvider } from "./context/Context";
 import ProfilePage from "./Pages/ProfilePages/index";
 import Cookies from "js-cookie";
+
+import DriverRegistration from "./Pages/Registeration/DriverRegistration";
+import UserRegistration from "./Pages/Registeration/UserRegistration";
+
 axios.defaults.baseURL="http://localhost:8080";
 axios.defaults.withCredentials=true;
 function App() {
@@ -48,33 +52,35 @@ function App() {
   };
   return (
     <BrowserRouter>
-    <UserContextProvider>
-      <Navbar
-        classDisplay={classDisplay}
-        setClassDislay={setClassDisplay}
-        displayVal={displayVal}
-        setDisplayVal={setDisplayVal}
-        setLoginState={setLoginState}
-        loginState={loginState}
-      />
-      <div style={intDivStyle}>
-        <Registeration />
-        {/* <Support/> */}
-        
-        <Routes>
-          <Route
-            path="/"
-            element={<Homepage nonceVal={nonce} loginState={loginState} />}
-          />
-          <Route path="/support" element={<Support />} />
-          <Route path="/maps" element={<MapLayout nonceVal={nonce} />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          {/* <Route path="/userprofile" element={<UserProfile />} /> */}
-          <Route path="/registration" element={<Registeration />} />
-          <Route path="/profile" element={<ProfilePage />} />
-        </Routes>
-        <Footer />
-      </div>
+      <UserContextProvider>
+        <Navbar
+          classDisplay={classDisplay}
+          setClassDislay={setClassDisplay}
+          displayVal={displayVal}
+          setDisplayVal={setDisplayVal}
+          setLoginState={setLoginState}
+          loginState={loginState}
+        />
+        <div style={intDivStyle}>
+          <Registeration />
+          {/* <Support/> */}
+
+          <Routes>
+            <Route
+              path="/"
+              element={<Homepage nonceVal={nonce} loginState={loginState} />}
+            />
+            <Route path="/support" element={<Support />} />
+            <Route path="/maps" element={<MapLayout nonceVal={nonce} />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            {/* <Route path="/userprofile" element={<UserProfile />} /> */}
+            <Route path="/driverregistration" component={DriverRegistration} />
+            <Route path="/userregistration" component={UserRegistration} />
+            <Route path="/registration" element={<Registeration />} />
+            <Route path="/profile" element={<ProfilePage />} />
+          </Routes>
+          <Footer />
+        </div>
       </UserContextProvider>
     </BrowserRouter>
   );
