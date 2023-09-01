23406ede0ec1e8fd96ca737deccd49baa72ee199
Aditya Kumar <ceo@sameerswain.com>
2023-09-01 16:13:40 +0530

    correction in navigation

--- grabway/src/Pages/MatchedRoutes/MatchedRoutes.jsx
+++ grabway/src/Pages/MatchedRoutes/MatchedRoutes.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Navigate } from "react-router-dom"; 
 import { useLocation, useParams } from 'react-router-dom';
+import Cookies from 'js-cookie';
 const MatchedRoutes = () => {
     const [matchedRoutes,setMatchedRoutes]=useState(null);
     const [driverFound, setDriverFound]=useState(false);
@@ -22,6 +24,27 @@ const MatchedRoutes = () => {
         searchData();
     },[])
 
+    if(location.state===null){
+        return <Navigate to='/'/>
+    }
+    const userData=Cookies.get('grabwayUser');
+    const googleUserData=Cookies.get('grabwayGoogleToken')
+    if (userData !== undefined) {
+        if (JSON.parse(userData).name === "") {
+          return <Navigate to={"/registration"} userType="" />;
+        }
+        if (JSON.parse(userData).userType === "driver") {
+          return <Navigate to={"/"} />;
+        }
+      }
+      if (!userData) {
+        if (!googleUserData) {
+          return <Navigate to={"/"} />;
+        }
+        if (googleUserData) {
+          return <Navigate to={"/googleRegistration"} />;
+        }
+      }
   return (
     <>
      {matchedRoutes===null&& <>
