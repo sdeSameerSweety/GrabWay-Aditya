74a21a43961bc92075ec4a9f0474e39201cbc94c
Aditya Kumar <ceo@sameerswain.com>
2023-09-03 02:06:04 +0530

    taking-data-dynamically-with-adv

--- grabway/src/components/Cards/DriverCards/DriverCards.jsx
+++ grabway/src/components/Cards/DriverCards/DriverCards.jsx
@@ -18,83 +18,92 @@ import {
 import AdvertisementCard from "./AdvertisementCard/AdvertisementCard";
 import "./DriverCard.css";
 
+// Define your sections and other constants
 const sections = ["Essential Commuter", "Comfort Traveler", "Premier Business"];
 const areTabsDisabled = true;
-// const cardData = {};
+
+// Simulated driver data (replace with actual data fetching)
+const driverData = [
+  {
+    driverName: "Kittu Singh",
+    email: "driver@gmail.com",
+    VehicleManufacturer: "Maruti Suzuki",
+    VehicleModel: "Swift Dzire",
+    plan: "basic",
+    seats: 10,
+  },
+  {
+    driverName: "John Doe",
+    email: "john.doe@gmail.com",
+    VehicleManufacturer: "Toyota",
+    VehicleModel: "Camry",
+    plan: "premium",
+    seats: 5,
+  },
+  {
+    driverName: "Alice Johnson",
+    email: "alice.johnson@gmail.com",
+    VehicleManufacturer: "Honda",
+    VehicleModel: "Civic",
+    plan: "standard",
+    seats: 4,
+  },
+  {
+    driverName: "Michael Smith",
+    email: "michael.smith@gmail.com",
+    VehicleManufacturer: "Ford",
+    VehicleModel: "F-150",
+    plan: "basic",
+    seats: 6,
+  },
+  {
+    driverName: "Emily Wilson",
+    email: "emily.wilson@gmail.com",
+    VehicleManufacturer: "Chevrolet",
+    VehicleModel: "Malibu",
+    plan: "standard",
+    seats: 4,
+  },
+];
+
+// ... your existing imports and code ...
+
 function DriverCard() {
-  const [driverData, setDriverData] = useState({
-    driverName: "",
-    email: "",
-    VehicleManufacturer: "",
-    VehicleModel: "",
-    plan: "",
-    seats: 0,
-  });
-
-  const fetchDriverData = async () => {
-    try {
-      const response = await fetch("/api/driver");
-      const data = await response.json();
-
-      setDriverData(data);
-    } catch (error) {
-      console.error("Error fetching driver data: ", error);
-    }
-  };
-
-  // Fetch driver data when the component mounts
-  useEffect(() => {
-    fetchDriverData();
-  }, []);
-
+  // Render the component with the fetched data
   return (
     <Box p={8} borderRadius="md" boxShadow="lg" bg="white">
       <Tabs defaultIndex={0} colorScheme="blue">
         <TabList justifyContent="center" borderBottomWidth="1px" pb={2}>
-          {sections.map((section, index) => (
-            <Tab
-              key={index}
-              isDisabled={
-                areTabsDisabled &&
-                (section === "Comfort Traveler" ||
-                  section === "Premier Business")
-              }
-              _selected={{
-                color: "blue.500",
-                borderBottomWidth: "2px",
-              }}
-            >
-              {section}
-            </Tab>
-          ))}
+          {/* ... your existing TabList mapping code ... */}
         </TabList>
         <TabPanels mt={4}>
           {sections.map((section, index) => (
             <TabPanel key={index}>
-              {/* Display driver information */}
-              <Box className="responsive-card">
-                <Image
-                  src="https://img.freepik.com/free-vector/red-sedan-car-isolated-white-vector_53876-64366.jpg?w=900&t=st=1692895124~exp=1692895724~hmac=9084cfe1f9e13be10c20ff75317e808efbecd741a345ac619f7814540a6abf66"
-                  alt="Driver Image"
-                  objectFit="cover"
-                  borderRadius="md"
-                  w={400}
-                />
-                <div className="card-content">
-                  <Heading size="lg">{driverData.driverName}</Heading>
-                  <Text>Email: {driverData.email}</Text>
-                  <Text>
-                    Vehicle: {driverData.VehicleManufacturer}{" "}
-                    {driverData.VehicleModel}
-                  </Text>
-                  <Text>Plan: {driverData.plan}</Text>
-                  <Text>Seats: {driverData.seats}</Text>
-                </div>
-                <Box mx={6} my={4} className="sideCardCss">
-                  <AdvertisementCard />
+              {driverData.map((driver, driverIndex) => (
+                <Box className="responsive-card" key={driverIndex}>
+                  <Image
+                    src="https://img.freepik.com/free-vector/red-sedan-car-isolated-white-vector_53876-64366.jpg?w=900&t=st=1692895124~exp=1692895724~hmac=9084cfe1f9e13be10c20ff75317e808efbecd741a345ac619f7814540a6abf66"
+                    alt="Driver Image"
+                    objectFit="cover"
+                    borderRadius="md"
+                    w={400}
+                  />
+                  <div className="card-content">
+                    <Heading size="lg">{driver.driverName}</Heading>
+                    <Text>Email: {driver.email}</Text>
+                    <Text>
+                      Vehicle: {driver.VehicleManufacturer}{" "}
+                      {driver.VehicleModel}
+                    </Text>
+                    <Text>Plan: {driver.plan}</Text>
+                    <Text>Seats: {driver.seats}</Text>
+                  </div>
+                  <Box mx={6} my={4} className="sideCardCss">
+                    {/* Conditionally render AdvertisementCard only for the first driver */}
+                    {driverIndex === 0 && <AdvertisementCard />}
+                  </Box>
                 </Box>
-              </Box>
-              {/* Rest of your component code for services */}
+              ))}
             </TabPanel>
           ))}
         </TabPanels>
