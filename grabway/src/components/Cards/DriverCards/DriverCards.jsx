6f88084da6b3b6123d0c4e0312b3931cb27647d8
Aditya Kumar <ceo@sameerswain.com>
2023-09-03 02:34:45 +0530

    taking-data-dynamically-with-adv-2

--- grabway/src/components/Cards/DriverCards/DriverCards.jsx
+++ grabway/src/components/Cards/DriverCards/DriverCards.jsx
@@ -1,4 +1,4 @@
-import { React, useState, useEffect } from "react";
+import React from "react";
 import {
   Box,
   Button,
@@ -67,43 +67,59 @@ const driverData = [
 function DriverCard() {
   return (
     <Box p={8} borderRadius="md" boxShadow="lg" bg="white">
-      <Box className="driver-card-container">
-        {driverData.map((driver, driverIndex) => (
-          <Box
-            key={driverIndex}
-            borderRadius="md"
-            boxShadow="md"
-            bg="white"
-            p={4}
-            m={2}
-            textAlign="center"
-            width="300px" // Set a fixed width for each card
-            position="relative"
-          >
-            <Image
-              src="https://img.freepik.com/free-vector/red-sedan-car-isolated-white-vector_53876-64366.jpg?w=900&t=st=1692895124~exp=1692895724~hmac=9084cfe1f9e13be10c20ff75317e808efbecd741a345ac619f7814540a6abf66"
-              alt="Driver Image"
-              objectFit="cover"
-              borderRadius="md"
-              w={200}
-              h={120}
-              mx="auto"
-            />
-            <Heading size="md">{driver.driverName}</Heading>
-            <Text>Email: {driver.email}</Text>
-            <Text>
-              Vehicle: {driver.VehicleManufacturer} {driver.VehicleModel}
-            </Text>
-            <Text>Plan: {driver.plan}</Text>
-            <Text>Seats: {driver.seats}</Text>
-            {driverIndex === 0 && (
-              <Box position="absolute" top={2} right={2} zIndex={1}>
-                <AdvertisementCard />
+      <Tabs defaultIndex={0} colorScheme="blue" isLazy={areTabsDisabled}>
+        <TabList justifyContent="center" borderBottomWidth="1px" pb={2}>
+          {sections.map((section, index) => (
+            <Tab key={index} isDisabled={index > 0 && areTabsDisabled}>
+              {section}
+            </Tab>
+          ))}
+        </TabList>
+        <TabPanels mt={4}>
+          {sections.map((section, index) => (
+            <TabPanel key={index}>
+              <Box className="driver-card-container">
+                {driverData.map((driver, driverIndex) => (
+                  <Box
+                    key={driverIndex}
+                    borderRadius="md"
+                    boxShadow="md"
+                    bg="white"
+                    p={4}
+                    m={2}
+                    textAlign="center"
+                    width="300px" // Set a fixed width for each card
+                    position="relative"
+                  >
+                    <Image
+                      src="https://img.freepik.com/free-vector/red-sedan-car-isolated-white-vector_53876-64366.jpg?w=900&t=st=1692895124~exp=1692895724~hmac=9084cfe1f9e13be10c20ff75317e808efbecd741a345ac619f7814540a6abf66"
+                      alt="Driver Image"
+                      objectFit="cover"
+                      borderRadius="md"
+                      w={200}
+                      h={120}
+                      mx="auto"
+                    />
+                    <Heading size="md">{driver.driverName}</Heading>
+                    <Text>Email: {driver.email}</Text>
+                    <Text>
+                      Vehicle: {driver.VehicleManufacturer}{" "}
+                      {driver.VehicleModel}
+                    </Text>
+                    <Text>Plan: {driver.plan}</Text>
+                    <Text>Seats: {driver.seats}</Text>
+                    {driverIndex === 0 && (
+                      <Box position="absolute" top={2} right={2} zIndex={1}>
+                        <AdvertisementCard />
+                      </Box>
+                    )}
+                  </Box>
+                ))}
               </Box>
-            )}
-          </Box>
-        ))}
-      </Box>
+            </TabPanel>
+          ))}
+        </TabPanels>
+      </Tabs>
     </Box>
   );
 }
