6da711228f8a7a74215269a820d8b58582560f2d
Aditya Kumar <ceo@sameerswain.com>
2023-09-04 20:59:58 +0530

    added-mediaquries-for-loader

--- grabway/src/App.js
+++ grabway/src/App.js
@@ -27,6 +27,7 @@ import RouteUserRegisteration from "./Pages/RouteRegisteration/RouteUserRegistra
 import MatchedRoutes from "./Pages/MatchedRoutes/MatchedRoutes";
 import UserPackages from "./Pages/UserPackages/UserPackages";
 import MoreDetails from "./Pages/MoreDetails/MoreDetails";
+import LoadingCarAnimation from "./components/AllAnimations/LoadingCarAnimation";
 axios.defaults.baseURL = "http://localhost:8080";
 axios.defaults.withCredentials = true;
 // import NearbyMap from "./components/Map/NearbyMap";
@@ -90,6 +91,7 @@ function App() {
         />
 
         <div style={intDivStyle}>
+          <LoadingCarAnimation />
           <Routes>
             {/* <Route path="/nearby" element={<NearbyMap nonceVal={nonce} />} /> */}
             <Route
