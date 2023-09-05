c429f3b395c7bbc8f64c4c50b8c4b72f9f5d4acc
Aditya Kumar <ceo@sameerswain.com>
2023-09-06 02:28:01 +0530

    partiallly-done-3

--- grabway/src/Pages/ViewMoreDeatails/DriverMoreDetails.jsx
+++ grabway/src/Pages/ViewMoreDeatails/DriverMoreDetails.jsx
@@ -1,6 +1,18 @@
 // DriverDetails.js
 import React from "react";
-import "./DriverMoreDetails.css"; // Import the CSS file
+import {
+  Box,
+  Image,
+  Text,
+  VStack,
+  HStack,
+  Badge,
+  Divider,
+  Grid,
+  GridItem,
+  Card,
+} from "@chakra-ui/react";
+import "./DriverDetails.css"; // Import the CSS file
 
 const DriverDetails = () => {
   // Realistic driver and route data for a driver in Bhubaneswar, Odisha
@@ -53,9 +65,17 @@ const DriverDetails = () => {
   };
 
   return (
-    <div className="driver-details-container">
+    <Box
+      p={6}
+      borderWidth="1px"
+      borderRadius="lg"
+      boxShadow="lg"
+      bg="white"
+      color="gray.800"
+      className="container-driver-view-details"
+    >
       <div className="profile-header">
-        <img
+        <Image
           src={driver.profileImage}
           alt={`Profile of ${driver.name}`}
           className="profile-image"
@@ -67,11 +87,12 @@ const DriverDetails = () => {
           <p>Vehicle Number: {driver.vehicleNumber}</p>
           <p>Car Type: {driver.carType}</p>
           <div className="driver-rating">
-            <span className="badge">Top Rated</span>
+            <Badge colorScheme="green">Top Rated</Badge>
             <p>Rating: {driver.rating}</p>
           </div>
         </div>
       </div>
+      <Divider borderColor="gray.300" />
       <div className="route-info">
         <h3>Route Information</h3>
         <p>From: {route.from}</p>
@@ -81,21 +102,29 @@ const DriverDetails = () => {
         <p>Total Seats: {driver.totalSeats}</p>
         <p>Total Fare: {driver.totalFare}</p>
       </div>
-      <div className="passenger-list">
-        {route.passengers.map((passenger) => (
-          <div key={passenger.id} className="customer-card">
-            <h4>Customer Information</h4>
-            <p>Name: {passenger.name}</p>
-            <p>Email: {passenger.email}</p>
-            <p>Phone: {passenger.phone}</p>
-            <p>From: {passenger.from}</p>
-            <p>To: {passenger.to}</p>
-            <div className="details-divider"></div>
-            <p>{passenger.details}</p>
-          </div>
-        ))}
+      <Divider borderColor="gray.300" />
+      <div className="customer-card">
+        <h4>Customers Information</h4>
+        <Grid templateColumns={{ sm: "1fr", md: "repeat(3, 1fr)" }} gap={4}>
+          {route.passengers.map((passenger) => (
+            <GridItem key={passenger.id}>
+              <Card>
+                <VStack align="start">
+                  {/* <h4>Customer Information</h4> */}
+                  <p>Name: {passenger.name}</p>
+                  <p>Email: {passenger.email}</p>
+                  <p>Phone: {passenger.phone}</p>
+                  <p>From: {passenger.from}</p>
+                  <p>To: {passenger.to}</p>
+                  <Divider my={1} borderColor="gray.300" />
+                  <p>{passenger.details}</p>
+                </VStack>
+              </Card>
+            </GridItem>
+          ))}
+        </Grid>
       </div>
-    </div>
+    </Box>
   );
 };
 
